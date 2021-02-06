30df65283ee6ed71fd88ac49c291da5efab736fd
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-06 08:04:01 -0500

    added user_params and order_params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,20 @@
 class OrdersController < ApplicationController
     def create
+        user_params
+        order_params
         byebug
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :address)
+    end
+
+    def order_params
+        params.require(:order)
+        .permit(:total, :items => [:name, :cost, :icon, :id])
+    end
+
+    
 end
