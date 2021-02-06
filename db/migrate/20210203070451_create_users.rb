940b04b8d0fd5dc142cfd5a366ff210f3c84076e
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-06 08:02:59 -0500

    db reset

--- db/migrate/20210203070451_create_users.rb
+++ db/migrate/20210203070451_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :firstname
+      t.string :lastname
       t.string :email
       t.text :address
 
--- db/migrate/20210203070640_create_items.rb
+++ db/migrate/20210203070640_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :name
-      t.text :pic_url
+      t.text :icon
       t.integer :price
 
       t.timestamps
