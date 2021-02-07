c3ea2ecfc9d502c645a30642eb20b706acebaea7
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-07 02:33:59 -0500

    added json to create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,13 +13,14 @@ class OrdersController < ApplicationController
     end
 
     def create
-        @user = User.find_or_create_by(user_params)
-        @order = Order.create(total: order_params[:total])
-        @order.user = @user
-        @items = Item.set_items(order_params[:items])
-        @order.items = @items
-        @order.save
-        byebug
+        user = User.find_or_create_by(user_params)
+        order = Order.create(total: order_params[:total])
+        order.user = user
+        items = Item.set_items(order_params[:items])
+        order.items = items
+        order.save
+
+        render json: order
     end
 
     private
