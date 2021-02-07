fa583a577e5f59d5aa952f59db34a0cb0dc3eb55
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-07 01:21:12 -0500

    added function to save each Item from params

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
     has_many :orders
     has_many :users, through: :orders
 
-    def self.set_items(items)
-        items.collect do |item|
+    def self.set_items(items_params)
+        items_params.collect do |item|
             item = Item.find_or_create_by(item)
             item.save
             item
